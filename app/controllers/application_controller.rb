782316db36b0390d2859f986a7405d995428ee0a
danmada <danmada@gmail.com>
2021-08-02 12:42:10 -0500

    commit tree 84015ee0e661cbc40be7a34366d8c55a059ec698

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,14 @@
 class ApplicationController < Sinatra::Base
+  set :default_content_type, 'application/json'
 
-  # add routes
+  get '/bakeries' do
+    bakeries = Bakery.all
+    bakeries.to_json
+  end
+
+  get '/bakeries/:id' do 
+    bakeries = Bakery.find(params[:id])
+    bakeries.to_json(include: :baked_goods)
+  end
 
 end
